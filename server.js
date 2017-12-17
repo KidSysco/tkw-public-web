ef12bca5a6379b8baf45a2424b567751319d28cf
KidSysco <KidSysco@KidSyscos-iMac.local>
2017-12-16 22:00:37 -0600

    setting up server to run over port 80 for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express = require('express'),
     app = module.exports = express(),
-    webServerPort = 8080;
+    webServerPort = 80;
 
 app.use(express.static('public'));
 //app.get('/', (req, res) => res.send('Hello World!'));
