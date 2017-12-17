cd5c479c97e70122534e9a4290f9dcc2e8b2b52b
KidSysco <KidSysco@KidSyscos-iMac.local>
2017-12-16 22:11:26 -0600

    heroku port 5000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express = require('express'),
     app = module.exports = express(),
-    webServerPort = 443;
+    webServerPort = 5000;
 
 app.use(express.static('public'));
 //app.get('/', (req, res) => res.send('Hello World!'));
