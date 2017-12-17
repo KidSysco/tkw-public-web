f93f0312482115f0e72165cb1b16a0f908110577
KidSysco <KidSysco@KidSyscos-iMac.local>
2017-12-16 22:15:15 -0600

    trying more heroku settings

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ var express = require('express'),
     app = module.exports = express(),
     webServerPort = 5000;
 
+app.set('port', (process.env.PORT || webServerPort))
+
 app.use(express.static('public'));
 //app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(webServerPort);
+app.listen(app.get('port'));
 
 console.log('Listening on port ' + webServerPort);
